a52d38a9c778781c18cedb64af0011d38008aa4a
KeiferW <89943031+KeiferW@users.noreply.github.com>
2021-09-09 15:31:37 -0400

    Add files via upload

---
+++ AddUser.php
@@ -0,0 +1,41 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Login = $inData["Login"];
+	$Password = $inData["Password"];
+
+	$conn = new mysqli("localhost", "Admin", "Administrator", "project");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $inData["Login"], $inData["Password"]);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
